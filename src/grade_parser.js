d1279b257e0bf71d6fab6dc2e3a972de37de2d94
bud aminof <budaminof@gmail.com>
2016-06-06 10:17:59 -0600

    passed first test- getMovements

--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,5 +1,13 @@
 exports.getMovements = function (grades) {
+  var result = [];
+  for (var i = 0; i < grades.length; i++) {
+    if ( grades[i+1] > grades[i] ) result.push("up")
+    if ( grades[i+1] < grades[i] ) result.push("down")
+    if ( grades[i+1] === grades[i] ) result.push("even")
+  }
+  return result;
 }
 
 exports.inDecline = function (grades) {
+  
 }
