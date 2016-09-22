feebfaf0755009df314d5fe59fc18d8957485899
bud aminof <budaminof@gmail.com>
2016-09-22 14:40:38 -0600

    grades with a gradesArr function

--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,5 +1,28 @@
 exports.getMovements = function (grades) {
+  return gradesArr(grades);
 }
 
 exports.inDecline = function (grades) {
+  let checker = 0;
+  let newGrades = gradesArr(grades);
+
+  for (let i = 0; i < newGrades.length; i++) {
+    if ( newGrades[i] == "down" ) checker++;
+    if ( newGrades[i] == "up" ) checker = 0;
+  }
+
+  return checker >= 3 ? true : false;
+}
+
+function gradesArr (grades) {
+
+  let result = [];
+
+  for (let i = 0; i < grades.length; i++) {
+    if ( grades[i+1] > grades[i] ) result.push("up");
+    if ( grades[i+1] < grades[i] ) result.push("down");
+    if ( grades[i+1] == grades[i] ) result.push("even");
+  }
+
+  return result;
 }
